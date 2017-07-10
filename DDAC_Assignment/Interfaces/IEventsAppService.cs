69312998961de5d999e35535179a589901b011a7
Chew Lee Yang <Chew Lee Yang>
2017-07-10 22:01:08 +0800

    Add project files.

---
+++ DDAC_Assignment/Interfaces/IEventsAppService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using DDAC_Assignment.Models;
+
+namespace DDAC_Assignment.Interfaces
+{
+    public interface IEventsAppService
+    {
+        Task<IEnumerable<ActiveDirectoryUser>> ActiveDirectoryUsersAsync();
+    }
+}
---
+++ DDAC_Assignment/Models/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDAC_Assignment.Models
+{
+    public class Booking
+    {
+        public string bookingId { get; set; }
+        public string scheduleId { get; set; }
+        public string bookingDate { get; set; }
+    }
+}
